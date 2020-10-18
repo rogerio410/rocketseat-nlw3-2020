45c507d24736112f6225dcf70501eefffab6c1ff
Rogerio Silva <rogerio410@gmail.com>
2020-10-18 14:50:04 -0300

    Mobile added ok

---
+++ mobile/src/pages/OrphangesMap.tsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from 'react'
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
+
+import { Feather } from '@expo/vector-icons'
+
+import mapMarker from '../images/map-marker.png'
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import api from '../services/api';
+
+interface Orphanage {
+    id: number,
+    name: string,
+    latitude: number,
+    longitude: number
+}
+
+export default function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([])
+    const navigation = useNavigation()
+
+    useFocusEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data)
+        })
+    })
+
+    function handleNavigateToOrphanageDetails(id: number) {
+        navigation.navigate('OrphanageDetails', { id })
+    }
+
+    function handleNavigateToCreateOrphanage() {
+        navigation.navigate('SelectMapPosition')
+    }
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                initialRegion={{
+                    latitude: -5.0677998,
+                    longitude: -42.7897404,
+                    latitudeDelta: 0.008,
+                    longitudeDelta: 0.008,
+                }}
+            >
+                {orphanages.map(orphanage => {
+                    return (
+                        <Marker
+                            key={orphanage.id}
+                            calloutAnchor={{
+                                x: 2.8,
+                                y: 0.9,
+                            }}
+                            icon={mapMarker}
+                            coordinate={{
+                                latitude: orphanage.latitude,
+                                longitude: orphanage.longitude,
+                            }}
+                        >
+                            <Callout tooltip={true} onPress={() => handleNavigateToOrphanageDetails(orphanage.id)}>
+                                <View style={styles.calloutContainer}>
+                                    <Text style={styles.calloutText}>{orphanage.name}</Text>
+                                </View>
+                            </Callout>
+                        </Marker>
+                    )
+                })}
+
+            </MapView>
+
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>
+                    {orphanages.length} orfanatos encontrados..
+        </Text>
+                <RectButton
+                    style={styles.createOrphanagesButton}
+                    onPress={handleNavigateToCreateOrphanage}>
+                    <Feather name="plus" size={20} color="#fff" />
+                </RectButton>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    calloutContainer: {
+        width: 160,
+        height: 46,
+        paddingHorizontal: 16,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 16,
+        justifyContent: "center",
+    },
+    calloutText: {
+        fontFamily: 'Nunito_700Bold',
+        color: '#0089a5',
+        fontSize: 14,
+    },
+    footer: {
+        position: 'absolute',
+        left: 24,
+        right: 24,
+        bottom: 32,
+        backgroundColor: '#FFF',
+        borderRadius: 20,
+        height: 56,
+        paddingLeft: 20,
+        paddingRight: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: "center",
+        elevation: 3
+    },
+    footerText: {
+        fontFamily: 'Nunito_700Bold',
+        color: '#8fa7b3',
+    },
+    createOrphanagesButton: {
+        width: 56,
+        height: 46,
+        backgroundColor: '#15c3d6',
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
---
+++ mobile/src/services/api.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://192.168.0.102:3333/', // if IOS jsut http://localhost:333, android-emulador: adb reverse tcp:3333 tcp:3333 
+})
+
+export default api
